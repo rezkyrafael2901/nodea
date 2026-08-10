e9d69460dd859963e3716c58e9e96f1b2bce2b83
Rezki <redoaldi34@gmail.com>
2026-08-10 17:29:22 +0800

    header: sticky wrapper around navbar (fixed layout structure)

--- src/app/page-client.tsx
+++ src/app/page-client.tsx
@@ -1291,6 +1291,7 @@ export default function PageClient() {
             </AnimatePresence>
           </div>
         </motion.header>
+        </div>
 
         <main className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${view === "article" ? "py-0" : "py-12 md:py-20 lg:py-28"}`}>
           {view === "home" && (
